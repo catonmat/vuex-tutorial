396a2e875b171ca5b89a262328c5c115fde17e13
samdbeckham <sbeckham@gitlab.com>
2017-12-04 17:25:33 +0000

    :heavy_plus_sign: Added the store and created a card module

---
+++ store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import cards from './modules/cards';
+
+Vue.use(Vuex);
+
+// const debug = process.env.NODE_ENV !== 'production'
+const debug = false;
+
+export default new Vuex.Store({
+  modules: {
+    cards
+  },
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
+})
---
+++ store/modules/cards.js
@@ -0,0 +1,10 @@
+// Initial state
+const state = {
+  'cardone': {
+    text: 'I am the first card'
+  }
+};
+
+export default {
+  state
+};
