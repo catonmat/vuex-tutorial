160970deca68302bd6c6e1cabc51517576cf1351
samdbeckham <sbeckham@gitlab.com>
2018-06-13 16:56:22 +0100

    :shower: Cleans up the auth stuff and makes it beghave a lot nicer

--- components/Auth.vue
+++ components/Auth.vue
@@ -1,23 +1,37 @@
 <template>
-    <div>
-      <button @click="banana()">banana</button>
-      <button @click="logIn()">Log in</button>
+  <div class="auth">
+    <div v-if="loggedIn">
+      <span>{{ user.displayName }}</span>
+      <button @click="logOut()">Log Out</button>
     </div>
+    <button v-if="!loggedIn" @click="logIn()">Log in</button>
+  </div>
 </template>
 
 <script>
+  import { mapGetters, mapMutations } from "vuex";
+
   export default {
-    props: [],
     methods: {
-      logIn: function() {
-        this.$store.dispatch('logIn');
-      },
-      banana: function() {
-        this.$store.dispatch('banana');
-      }
+      ...mapMutations(['logIn', 'logOut'])
+    },
+    computed: {
+      ...mapGetters(['user', 'loggedIn'])
+    },
+    beforeCreate() {
+      this.$store.dispatch('fetchCreds');
     }
   };
 </script>
 
 <style scoped>
+  .auth {
+    font-family: Arial, Helvetica, sans-serif;
+    position: fixed;
+    top: 0;
+    right: 0;
+    padding: .5em 1em;
+    background: #ccc;
+    z-index: 99;
+  }
 </style>
--- store/modules/auth.js
+++ store/modules/auth.js
@@ -1,38 +1,41 @@
 import firebase from "../../data/firebase";
 
 const state = {
-  user: { userId: 1234 }
+  user: {}
 };
 
 export const getters = {
-  getUser: state => {
-    return state.user
-  }
+  user: state => state.user,
+  loggedIn: state => 'uid' in state.user
 };
 
 export const mutations = {
   setUser(state, payload) {
     state.user = payload;
-  }
-};
-
-export const actions = {
-  banana({ commit }) {
+  },
+  logIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithRedirect(provider);
   },
-  logIn({ commit }) {
-    // Log in and get user data from firebase
-    firebase.auth().getRedirectResult().then(function(result) {
-        const { displayName, email, photoURL, uid } = result.user;
-        const user = { displayName, email, photoURL, uid };
-        commit('setUser', user);
-      }).catch(function(error) {
-        // TODO: Actually handle this error
-        debugger;
-      });
-    }
+  logOut() {
+    firebase.auth().signOut();
+  }
+};
+
+export const actions = {
+  fetchCreds({ commit }) {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        const { displayName, email, photoURL, uid } = user;
+        const cleanedUser = { displayName, email, photoURL, uid };
+
+        commit('setUser', cleanedUser);
+      } else {
+        commit('setUser', {});
+      }
+    });
+  }
 }
 
 export default {
